ae2e60c8e35d248b05b2be64e1b32168b5dc7363
jawomast <sbeck.net@gmail.com>
2023-02-12 02:24:01 +0100

    section 2 complette

---
+++ basic/resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>About Page</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <h1>About Page from Controller</h1>
+        <a href="{{ route('contact.page') }}">Contact</a>
+    </body>
+</html>
---
+++ basic/resources/views/contact.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Contact Page</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <h2>Contact Page from Controller</h2>
+        <a href="{{ route('about.page') }}">About</a>
+    </body>
+</html>
