effd3abfdaa0579ae0e55b8529ea709fa35c0e2e
jawomast <sbeck.net@gmail.com>
2023-02-20 20:50:19 +0100

    section3

--- basic/routes/web.php
+++ basic/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Demo\DemoController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(DemoController::class)->group(function(){
+    Route::get('/about', 'index')->name('about.page')->middleware('check');
+    Route::get('/contact', 'contactMethod')->name('contact.page');
 });
 
 Route::get('/dashboard', function () {
