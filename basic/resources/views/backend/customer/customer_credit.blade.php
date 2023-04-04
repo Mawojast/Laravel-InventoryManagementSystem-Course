a797c7965607362d6aabd89d30002244c28ecd01
jawomast <sbeck.net@gmail.com>
2023-04-04 21:03:46 +0200

    complete section37 - Manage Customer Invoice Details

--- basic/resources/views/backend/customer/customer_credit.blade.php
+++ basic/resources/views/backend/customer/customer_credit.blade.php
@@ -47,8 +47,8 @@
                                                 <td>{{date('d-m-Y', strtotime($item['invoice']['date']))}}</td>
                                                 <td>{{$item->due_amount}}</td>
                                                 <td>
-                                                    <a href="{{ route('customer.edit.invoice',$item->id) }}" class="btn btn-info sm" title="Edit Data"><i class="fas fa-edit"></i></a>
-                                                    <a href="{{ route('customer.delete', $item->id)}}" class="btn btn-danger sm" title="Customer Invoice Details" id="delete"><i class="fas fa-eye"></i></a>
+                                                    <a href="{{ route('customer.edit.invoice',$item->invoice_id) }}" class="btn btn-info sm" title="Edit Data"><i class="fas fa-edit"></i></a>
+                                                    <a href="{{ route('customer.invoice.details.pdf', $item->invoice_id)}}" class="btn btn-danger sm" title="Customer Invoice Details" id="delete"><i class="fas fa-eye"></i></a>
                                                 </td>
                                             </tr>
                                                 @endforeach
